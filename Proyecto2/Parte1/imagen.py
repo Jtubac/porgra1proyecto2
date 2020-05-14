e22df7753a18259a0498d4346241293e94b8f903
Jtubac <63554133+Jtubac@users.noreply.github.com>
2020-05-13 20:27:31 -0600

    Add files via upload

---
+++ Proyecto2/Parte1/imagen.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+archivo=[]
+nuevo=[]
+archivo="lobo.jpg"
+nuevo="ejemplo_1.jpg"
+os.rename(archivo, nuevo)
+print([archivo], [nuevo])
+
+archivo="tienda.jpg"
+nuevo="ejemplo_2.jpg"
+os.rename(archivo, nuevo)
+print([archivo], [nuevo])
+
+archivo="sonic.jpg"
+nuevo="ejemplo_3.jpg"
+os.rename(archivo, nuevo)
+print([archivo], [nuevo])
+print()
+print("Ha realizado el cambio de nombre a los archivos")
+
+#with open('datos.json','w') as datos:    -----> no fucnciona
+#    json.dump(datos, archivo, nuevo)
+#    print("archivo exportado")
